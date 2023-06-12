65ccdd5090e05b8a144c4a227b6b5b419fbb2b8d
AsemDeve <120917153+AsemDeve@users.noreply.github.com>
2023-06-12 10:41:32 +0300

    fix figure two , directors top

--- analyzing1000TV_M.py
+++ analyzing1000TV_M.py
@@ -7,8 +7,8 @@ table_of_shows = pd.read_csv("imdb_top_1000.csv")
 # my_df=pd.DataFrame({table_of_shows})
 # print(my_df)
 # print(type(table_of_shows))
-
-brief_df = table_of_shows[['Series_Title', 'Genre', 'Director', 'IMDB_Rating']].copy()
+print(table_of_shows.columns)
+brief_df = table_of_shows[['Series_Title', 'Genre', 'Director', 'IMDB_Rating',"No_of_Votes"]].copy()
 
 
 # print(brief_df)
@@ -46,9 +46,9 @@ genre_counts_df = pd.DataFrame({'Genre': genre_counts.index, 'Count': genre_coun
 print(genre_counts_df)
 genre_counts_df = genre_counts_df.sort_values('Count', ascending=False)
 
-director_avg_ratings = brief_df.groupby('Director')['IMDB_Rating'].mean()
-director_ratings_df = pd.DataFrame({'Director': director_avg_ratings.index, 'IMDB_Rating': director_avg_ratings.values})
-director_ratings_df = director_ratings_df.sort_values('IMDB_Rating', ascending=False)
+director_avg_ratings = brief_df.groupby('Director')['No_of_Votes'].mean()
+director_ratings_df = pd.DataFrame({'Director': director_avg_ratings.index, 'No_of_Votes': director_avg_ratings.values})
+director_ratings_df = director_ratings_df.sort_values('No_of_Votes', ascending=False)
 top_10_directors = director_ratings_df.head(10)
 
 plt.figure(figsize=(12, 6))
@@ -59,13 +59,13 @@ plt.title('Most Popular Genres of Movies/TV Shows')
 plt.xticks(rotation=45)
 
 plt.figure(figsize=(12, 6))
-bar_plot = plt.bar(top_10_directors['Director'], top_10_directors['IMDB_Rating'])
-plt.bar_label(container=bar_plot, labels=top_10_directors['IMDB_Rating'])
+bar_plot = plt.bar(top_10_directors['Director'], top_10_directors['No_of_Votes'])
+plt.bar_label(container=bar_plot, labels=top_10_directors['No_of_Votes'])
 plt.xlabel('Director')
-plt.ylabel('IMDB Rating')
-plt.title('Top 10 Directors based on IMDB Rating of Movies/TV Shows')
+plt.ylabel('No_of_Votes')
+plt.title('Top 10 Directors based on No of Votes')
 plt.xticks(rotation=45)
-plt.ylim(bottom=8)
-plt.ylim(top=9)
+# plt.ylim(bottom=8)
+# plt.ylim(top=9)
 
 plt.show()
